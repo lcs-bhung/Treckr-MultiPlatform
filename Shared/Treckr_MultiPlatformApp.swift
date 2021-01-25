584485358c4c630a840d7d014ce89d2ebbb11e91
Branton Hung <BHung@lcs.on.ca>
2021-01-25 14:53:14 -0500

    Initial Commit

---
+++ Shared/Treckr_MultiPlatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  Treckr_MultiPlatformApp.swift
+//  Shared
+//
+//  Created by Branton Hung on 2021-01-25.
+//
+
+import SwiftUI
+
+@main
+struct Treckr_MultiPlatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
