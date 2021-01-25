c402ed58ec473d984d2dbc543b16342c43164056
Branton Hung <BHung@lcs.on.ca>
2021-01-25 16:52:43 -0500

    Added location store to location from JSON file

---
+++ Shared/Model/LocationStore.swift
@@ -0,0 +1,27 @@
+//
+//  LocationStore.swift
+//  Treckr MultiPlatform
+//
+//  Created by Branton Hung on 2021-01-25.
+//
+
+import Foundation
+class LocationStore: ObservableObject {
+    var places: [Location]
+    
+    init() {
+        //get the pointer to the file
+        let url = Bundle.main.url(forResource: "locations", withExtension: "json")!
+        
+        //Load the contents of the JSON file
+        let data = try! Data(contentsOf: url)
+        
+        //Convert files from JSON to an array
+        places = try! JSONDecoder().decode([Location].self, from: data)
+        
+        //Sort the list of locations in alphabetical order
+        places.sort(by: {$0.name < $1.name})
+        
+    }
+    
+}
