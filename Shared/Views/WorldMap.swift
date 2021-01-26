eda830d3350634412177e18f505238b560734aeb
Branton Hung <BHung@lcs.on.ca>
2021-01-26 08:52:32 -0500

    Got the Map view

--- Shared/Views/WorldMap.swift
+++ Shared/Views/WorldMap.swift
@@ -5,11 +5,18 @@
 //  Created by Branton Hung on 2021-01-26.
 //
 
+import MapKit
 import SwiftUI
 
 struct WorldMap: View {
+    
+    //Center of LCS, wide enough to show NA
+    @State var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 44.43922, longitude: -78.26571), span: MKCoordinateSpan(latitudeDelta: 40, longitudeDelta: 40))
+    
+    
     var body: some View {
-        Text("Hello, WorldMap")
+        Map(coordinateRegion: $region)
+            .navigationTitle("Map")
     }
 }
 
