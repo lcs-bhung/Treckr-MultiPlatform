9f7c3ca8ea1998c03070860366f4cfdd818a901a
Branton Hung <BHung@lcs.on.ca>
2021-01-26 11:53:51 -0500

    Made the tips list view however there is an error when loading the contents of the JSON file

--- Shared/Views/TipsList.swift
+++ Shared/Views/TipsList.swift
@@ -8,9 +8,38 @@
 import SwiftUI
 
 struct TipsList: View {
+    
+    let tips: [Tip]
+    
     var body: some View {
-        Text("Hello, TipsList")
+        
+        //If there are children on this thip show a label view
+        List(tips, id: \.text){ tip in
+            
+            if tip.children != nil {
+                Label(tip.text, systemImage: "quote.bubble")
+            } else {
+                
+                Text(tip.text)
+                
+            }
+            
+            
+        }
+        
     }
+    
+    init() {
+        //get a pointer to the file
+        let url = Bundle.main.url(forResource: "tips", withExtension: "json")
+        
+        //Load the contents of the JSON file
+        let data = try! Data(contentsOf: url)
+        
+        //Cnvert the data from JSON into an array
+        tips = try !JSONDecoder().decode([Tip.self], from: data)
+    }
+    
 }
 
 struct TipsList_Previews: PreviewProvider {
